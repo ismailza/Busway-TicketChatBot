b3928f7f38a847bcc684bb57a1e7030b94fc068d
Ismail ZAHIR <ismailza407@gmail.com>
2024-03-17 04:06:27 +0000

    setup project, #1

---
+++ src/main/java/ma/fstm/ilisi/buswayticketchatbot/BuswayTicketChatbotApplication.java
@@ -0,0 +1,13 @@
+package ma.fstm.ilisi.buswayticketchatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuswayTicketChatbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuswayTicketChatbotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/fstm/ilisi/buswayticketchatbot/BuswayTicketChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.fstm.ilisi.buswayticketchatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuswayTicketChatbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
